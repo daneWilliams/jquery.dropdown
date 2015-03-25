cc6cadaa4acb792baa8ee832a1b41c46f0735ea9
daneWilliams <dane@danewilliams.co.uk>
2015-03-25 10:45:39 +0000

    Version bump

--- js/jquery.dropdown.min.js
+++ js/jquery.dropdown.min.js
@@ -7,7 +7,7 @@
  *
  *	================================================================
  *
- *	@version		1.1.1
+ *	@version		1.1.2
  *
  *	@author			Dane Williams <dane@danewilliams.uk>
  *	@copyright		2014 Dane Williams
